4021df1d189d51a661b03dafd9764efd638d6def
Stasan-minecraft <119735945+Stasan-minecraft@users.noreply.github.com>
2024-09-04 20:54:46 +0300

    Add files via upload

--- server.sql
+++ server.sql
@@ -1,1 +1,18 @@
 
+-- Створення таблиці користувачів (users)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_pay BOOLEAN DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Створення таблиці сайтів (sites)
+CREATE TABLE sites (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    site_address VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
